dc18442a11a3b64f809572a6f90eda2465ced074
Natalie Wyll <nataliewyll@yahoo.com>
2017-07-18 12:05:05 -0500

    Checkpoint-9: adds animatePoints scoll event to landing.js

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,6 @@
-var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function() {
-
+var animatePoints = function(points) {
   var revealPoints = function() {
     for(var i = 0; i < points.length; i++) {
       points[i].style.opacity = 1;
@@ -14,3 +13,20 @@ var animatePoints = function() {
   revealPoints();
 
 };
+
+window.onload = function () {
+  //Automatically animate of screen is taller than 950 pixels
+  if(window.innerHeight > 950) {
+    animatePoints(pointsArray);
+  }
+
+  //Else animate on scroll
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+  window.addEventListener('scroll', function(event) {
+    if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+      animatePoints(pointsArray);
+    }
+  });
+}
